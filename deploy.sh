230f9976bf6243fd15d022f3e6b1eceb12fcc7f3
Glusk <8526099+Glusk@users.noreply.github.com>
2020-08-17 17:59:27 +0200

    Added debug lines to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 version=$(mvn help:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
+echo "Detected artifact version: $version"
+echo "Detected git TAG: $TRAVIS_TAG"
 # Only deploy if:
 # - this is a tagged version with a proper version number (for example 0.1.0)
 # - this is a snapshot version
-if { [ ! -z "$TRAVIS_TAG" ] && [[ $version =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; } || { [ -z "$TRAVIS_TAG" ] && [[ $version =~ ^[0-9]+.[0-9]+.[0-9]+-SNAPSHOT$ ]]; }
+if { [ -n "$TRAVIS_TAG" ] && [[ "$version" =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; } || { [ -z "$TRAVIS_TAG" ] && [[ "$version" =~ ^[0-9]+.[0-9]+.[0-9]+-SNAPSHOT$ ]]; }
 then
+  echo "Deploying..."
   gpg --fast-import .travis/gpg.asc
   mvn deploy -P publish -DskipTests=true --settings ".travis/maven-settings.xml"
 fi
