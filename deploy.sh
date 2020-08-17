28d0a530fe0437d195e23518b7ac42f311a305a7
Glusk <8526099+Glusk@users.noreply.github.com>
2020-08-17 12:49:59 +0200

    #5 Skip duplicate release deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-gpg --fast-import .travis/gpg.asc
-mvn deploy -P publish -DskipTests=true --settings ".travis/maven-settings.xml"
+version=$(mvn help:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
+# Only deploy if:
+# - this is a tagged version with a proper version number (for example 0.1.0)
+# - this is a snapshot version
+if { [ -z "$TRAVIS_TAG" ] && [[ $version =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; } || [[ $version =~ ^[0-9]+.[0-9]+.[0-9]+-SNAPSHOT$ ]]
+then
+  gpg --fast-import .travis/gpg.asc
+  mvn deploy -P publish -DskipTests=true --settings ".travis/maven-settings.xml"
+fi
