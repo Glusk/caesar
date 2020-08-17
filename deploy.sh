c0116398b6ab234b95cd14a4195745d8cb5ba2b2
Glusk <8526099+Glusk@users.noreply.github.com>
2020-08-17 14:17:35 +0200

    Corrected release condition

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ version=$(mvn help:evaluate -Dexpression=project.version | grep -Ev '(^\[|Downlo
 # Only deploy if:
 # - this is a tagged version with a proper version number (for example 0.1.0)
 # - this is a snapshot version
-if { [ -z "$TRAVIS_TAG" ] && [[ $version =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; } || [[ $version =~ ^[0-9]+.[0-9]+.[0-9]+-SNAPSHOT$ ]]
+if { [ ! -z "$TRAVIS_TAG" ] && [[ $version =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; } || { [ -z "$TRAVIS_TAG" ] && [[ $version =~ ^[0-9]+.[0-9]+.[0-9]+-SNAPSHOT$ ]]; }
 then
   gpg --fast-import .travis/gpg.asc
   mvn deploy -P publish -DskipTests=true --settings ".travis/maven-settings.xml"
