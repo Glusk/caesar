01363a2df1b70a2f0b4fceeb69920d4e7d14b5f3
Glusk <lj.slo@hotmail.com>
2020-08-24 10:47:19 +0200

    #37 Removed unneeded dependency

--- src/main/java/com.github.glusk.caesar/module-info.java
+++ src/main/java/com.github.glusk.caesar/module-info.java
@@ -2,8 +2,6 @@
  * The Caesar library Java 9 module.
  */
 module com.github.glusk.caesar {
-    requires jakarta.xml.bind;
-
     exports com.github.glusk.caesar;
     exports com.github.glusk.caesar.hashing;
 }
