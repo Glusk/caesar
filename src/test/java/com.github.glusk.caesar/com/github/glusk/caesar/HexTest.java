a2393d80922066193039a7d18762766bac5d3524
Glusk <lj.slo@hotmail.com>
2020-12-26 03:02:34 +0100

    #50 Implemented white space removal

--- src/test/java/com.github.glusk.caesar/com/github/glusk/caesar/HexTest.java
+++ src/test/java/com.github.glusk.caesar/com/github/glusk/caesar/HexTest.java
@@ -1,6 +1,7 @@
 package com.github.glusk.caesar;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -25,6 +26,13 @@ public class HexTest {
         assertArrayEquals(
             new byte[] {(byte) 0b10000000},
             new Hex("80").asArray()
+        );
+    }
+    @Test
+    public void discardsArgumentWhiteSpaces() {
+        assertEquals(
+            "0A0B0C0D",
+            new Hex("0A 0B\t0C\r\n0D").asHexString()
         );
     }
 }
