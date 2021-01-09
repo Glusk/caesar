fd0129fb1ee75353a1a87b829d99374ecf4ba8ff
Glusk <lj.slo@hotmail.com>
2021-01-09 20:17:55 +0100

    #54 Added Xor

---
+++ src/test/java/com.github.glusk.caesar/com/github/glusk/caesar/XorTest.java
@@ -0,0 +1,48 @@
+package com.github.glusk.caesar;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class XorTest {
+    @Test
+    public void conformsToXorTruthTable() {
+        assertArrayEquals(
+            new byte[] {1, 0, 0, 1},
+            new Xor(
+                () -> new byte[] {1, 0, 1, 0},
+                () -> new byte[] {0, 0, 1, 1}
+            ).asArray()
+        );
+    }
+    @Test
+    public void xorOfAnEmptyByteSequenceIsAnEmptyByteSequence() {
+        assertArrayEquals(
+            new byte[0],
+            new Xor(
+                () -> new byte[0],
+                () -> new byte[0]
+            ).asArray()
+        );
+    }
+    @Test
+    public void throwsIfArgumentsAreNotOfEqualLength() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> new Xor(
+                () -> new byte[0],
+                () -> new byte[1]
+            )
+        );
+    }
+    @Test
+    public void identityOperationCheck() {
+        assertArrayEquals(
+            new byte[] {1, 0, 1, 0},
+            new Xor(
+                () -> new byte[] {1, 0, 1, 0}
+            ).asArray()
+        );
+    }
+}
