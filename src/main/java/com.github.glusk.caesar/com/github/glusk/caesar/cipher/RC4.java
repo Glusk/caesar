c7f717a693d0a9f0b306db5eb876f8b91f31a9e9
Glusk <lj.slo@hotmail.com>
2023-04-10 08:46:15 +0200

    #64 Working implementation; simple tests pass!

--- src/main/java/com.github.glusk.caesar/com/github/glusk/caesar/cipher/RC4.java
+++ src/main/java/com.github.glusk.caesar/com/github/glusk/caesar/cipher/RC4.java
@@ -1,18 +1,25 @@
 package com.github.glusk.caesar.cipher;
 
 import com.github.glusk.caesar.Bytes;
+import com.github.glusk.caesar.Xor;
 
 public final class RC4 implements ImmutableCipher {
     /** The size of the state array (S). */
     private static final int STATE_ARRAY_SIZE = 256;
 
     private final byte[] S;
+    private final int i;
+    private final int j;
+    private final Bytes output;
 
     public RC4() {
-        this(new byte[STATE_ARRAY_SIZE]);
+        this(new byte[STATE_ARRAY_SIZE], 0, 0, Bytes.wrapped(new byte[0]));
     }
-    private RC4(final byte[] S) {
+    private RC4(final byte[] S, final int i, final int j, final Bytes output) {
         this.S = S;
+        this.i = i;
+        this.j = j;
+        this.output = output;
     }
 
     /**
@@ -41,14 +48,33 @@ public final class RC4 implements ImmutableCipher {
             S[i] = S[j];
             S[j] = temp;
         }
-        return new RC4(S);
+        return new RC4(S, 0, 0, Bytes.wrapped(new byte[0]));
     }
     @Override
-    public ImmutableCipher update(final Bytes message) {
-        throw new UnsupportedOperationException("Not Impl");
+    public ImmutableCipher update(final Bytes input) {
+        int inputLength = input.asArray().length;
+        byte[] K = new byte[inputLength];
+        byte[] S = this.S.clone();
+        for (int i = this.i, j = this.j, n = 0; n < inputLength; n++) {
+            // i := (i + 1) mod 256
+            // j := (j + S[i]) mod 256
+            i = (i + 1) % S.length;
+            j = (j + (S[i] & 0xff)) % S.length;
+            // swap values of S[i] and S[j]
+            byte temp = S[i];
+            S[i] = S[j];
+            S[j] = temp;
+            // K := S[(S[i] + S[j]) mod 256]
+            K[n] = (byte) S[((S[i] & 0xff) + (S[j] & 0xff)) % S.length];
+        }
+        return new RC4(
+            S, i, j,
+            new Xor(Bytes.wrapped(K), input)
+        );
     }
+
     @Override
     public Bytes doFinal() {
-        throw new UnsupportedOperationException("Not Impl");
+        return this.output;
     }
 }
